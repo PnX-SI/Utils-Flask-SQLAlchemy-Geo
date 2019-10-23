a468d284b1b7404198b18c83b025eeb6ef7e73c0
joel.clement <joel.clement@cevennes-parcnational.fr>
2019-10-23 16:04:04 +0200

    ajout from_geofeature

--- src/utils_flask_sqla_geo/serializers.py
+++ src/utils_flask_sqla_geo/serializers.py
@@ -1,12 +1,20 @@
 from geoalchemy2.shape import to_shape
 from geojson import Feature, FeatureCollection
-
+from utils_flask_sqla.serializers import serializable
+from shapely.geometry import asShape
+from geoalchemy2.shape import from_shape
+from geonature.utils.utilsgeometry import remove_third_dimension
 
 def geoserializable(cls):
     """
         Décorateur de classe
         Permet de rajouter la fonction as_geofeature à une classe
     """
+
+    # par defaut un geoserializable est aussi un serializable
+    # pas besoin de deux decorateurs
+
+    cls = serializable(cls)
 
     def serializegeofn(
         self, geoCol, idCol, recursif=False, columns=(), relationships=()
@@ -39,5 +47,35 @@ def geoserializable(cls):
         )
         return feature
 
+    def populategeofn(self, geojson, col_geom_name="geom"):
+        '''
+        Méthode qui initie les valeurs de l'objet SQLAlchemy à partir d'un geojson
+
+        Parameters
+        ----------
+            geojfeature_in : dictionnaire contenant les valeurs à passer à l'objet
+        '''
+
+        typeg = geojson.get('type')
+        properties = geojson.get('properties')
+        geometry = geojson.get('geometry')
+
+        if not properties or not geometry or typeg != "Feature":
+            raise GeonatureApiError(
+                "Input must be a geofeature"
+            )
+
+        # set properties
+        self.from_dict(properties)
+
+        # voir si meilleure procédure pour mettre la geometrie en base
+        shape = asShape(geometry)
+        two_dimension_geom = remove_third_dimension(shape)
+        geom = from_shape(two_dimension_geom, srid=4326)
+        setattr(self, col_geom_name, geom)
+
+
     cls.as_geofeature = serializegeofn
+    cls.from_geofeature = populategeofn
+
     return cls
