b9ea513d9140a6b062b182846e310d286a819d81
Élie Bouttier <elie.bouttier@ecrins-parcnational.fr>
2022-12-27 15:50:03 +0100

    Add GeoAlchemyAutoSchema marshmallow schema
    This schema supports models with geometric fields and geojson serialization/deserialization.

---
+++ src/utils_flask_sqla_geo/schema.py
@@ -0,0 +1,194 @@
+from enum import Enum
+
+from marshmallow import Schema, fields, RAISE, EXCLUDE
+from marshmallow.decorators import pre_load, post_dump
+from marshmallow.validate import OneOf, Range
+from marshmallow.exceptions import ValidationError
+
+from geoalchemy2 import Geometry
+from geoalchemy2.shape import to_shape, from_shape
+from marshmallow_sqlalchemy.schema import SQLAlchemyAutoSchema, SQLAlchemyAutoSchemaOpts
+from marshmallow_sqlalchemy.convert import ModelConverter
+from marshmallow_geojson import (
+    GeometryType,
+    PointSchema,
+    MultiPointSchema,
+    PolygonSchema,
+    MultiPolygonSchema,
+    LineStringSchema,
+    MultiLineStringSchema,
+)
+from shapely.geometry import shape
+
+
+class GeometrySchema(Schema):
+    schema_map = {
+        GeometryType.point.value: PointSchema,
+        GeometryType.multi_point.value: MultiPointSchema,
+        GeometryType.polygon.value: PolygonSchema,
+        GeometryType.multi_polygon.value: MultiPolygonSchema,
+        GeometryType.line_string.value: LineStringSchema,
+        GeometryType.multi_line_string.value: MultiLineStringSchema,
+    }
+
+    type = fields.Str(required=True, validate=OneOf(schema_map.keys()))
+
+    def load(self, data, *, many=None, **kwargs):
+        geometry_type = super().load(data, many=many, unknown=EXCLUDE)["type"]
+        schema = self.schema_map[geometry_type]
+        return schema(many=many, **kwargs).load(data)
+
+
+class FeatureSchema(Schema):
+    id = fields.Field()
+    type = fields.Constant("Feature", required=True)
+    # note: geometry validity done by GeometryField deserialization
+    geometry = fields.Mapping(required=True, allow_none=True)
+    properties = fields.Mapping(required=True)
+
+
+class FeatureCollectionSchema(Schema):
+    type = fields.Constant("FeatureCollection", required=True)
+    features = fields.List(fields.Nested(FeatureSchema), required=True)
+
+
+class GeometryField(fields.Field):
+    geometry_schema = GeometrySchema()
+
+    def _serialize(self, value, attr, obj):
+        return to_shape(value).__geo_interface__ if value else None
+
+    def _deserialize(self, value, attr, data, **kwargs):
+        try:
+            geom = shape(self.geometry_schema.load(value))
+            if not geom.is_valid:
+                raise ValidationError("Invalid geometry.")
+            if geom.has_z:
+                raise ValidationError("Unexpected third dimension.")
+            return from_shape(geom, srid=4326)
+        except ValueError as error:
+            raise ValidationError("Invalid geometry.") from error
+
+
+class GeoModelConverter(ModelConverter):
+    """Model converter for models with geometric fields."""
+
+    SQLA_TYPE_MAPPING = {
+        **ModelConverter.SQLA_TYPE_MAPPING,
+        Geometry: GeometryField,
+    }
+
+
+class GeoAlchemyAutoSchemaOpts(SQLAlchemyAutoSchemaOpts):
+    """Options class for ``GeoAlchemyAutoSchema``.
+    Adds the following options:
+
+    - ``geometry_fields``: List of Geometry columns.
+    - ``feature_id``: Identity field to use when generating features.
+    - ``feature_geometry``: Geometry field to use when generating features.
+
+    Thus, this options class define ``GeoModelConverter`` as default model converter.
+    """
+
+    def __init__(self, meta, *args, **kwargs):
+        super().__init__(meta, *args, **kwargs)
+        self.geometry_fields = set()
+        if self.model:
+            for column in self.model.__mapper__.columns:
+                if isinstance(column.type, Geometry):
+                    self.geometry_fields.add(column.key)
+        # TODO: if self.table: …
+        self.feature_id = getattr(meta, "feature_id", None)
+        if len(self.geometry_fields) == 1:
+            self.feature_geometry = getattr(
+                meta, "feature_geometry", self.geometry_fields.copy().pop()
+            )
+        else:
+            self.feature_geometry = getattr(meta, "feature_geometry", None)
+        self.model_converter = getattr(meta, "model_converter", GeoModelConverter)
+
+
+class GeoAlchemyAutoSchema(SQLAlchemyAutoSchema):
+    """Auto schema with support for geometric fields and geojson generation.
+
+    :param as_geojson: If ``true``, serialize and deserialize geojson instead of json.
+    :param feature_id: Identity field to use when generating features.
+        If ``None``, use ``feature_id`` specified on ``class Meta`` if any, otherwise
+        features are generated without id.
+    :param feature_geometry: Geometry field to use when generating features.
+        If ``None``, use ``feature_geometry`` specified on ``class Meta``.
+        If not specified on ``class Meta`` either, auto-detect the geometry field.
+        If none or several geometric fields are detected, raise a ``TypeError``.
+
+    Geometric fields are automatically removed from serialization.
+    """
+
+    OPTIONS_CLASS = GeoAlchemyAutoSchemaOpts
+
+    def __init__(
+        self,
+        *args,
+        as_geojson=False,
+        feature_id=None,
+        feature_geometry=None,
+        only=None,
+        exclude=(),
+        **kwargs
+    ):
+        excluded_geometry_fields = self.opts.geometry_fields.copy()
+        if only is not None:
+            only = set(only)
+            excluded_geometry_fields -= set(only)
+        exclude = set(exclude) | excluded_geometry_fields
+        self.as_geojson = as_geojson
+        if as_geojson:
+            self.feature_id = feature_id or self.opts.feature_id
+            self.feature_geometry = feature_geometry or self.opts.feature_geometry
+            if not self.feature_geometry:
+                raise TypeError("Missing 'feature_geometry'")
+            # Add feature geometry to serialized fields
+            exclude.discard(self.feature_geometry)
+            if only is not None:
+                only = set(only) | {self.feature_geometry}
+        super().__init__(*args, only=only, exclude=exclude, **kwargs)
+
+    def to_feature(self, properties):
+        feature = {
+            "properties": properties,
+            "geometry": properties.pop(self.feature_geometry),
+        }
+        if self.feature_id and self.feature_id in properties:
+            feature.update(
+                {
+                    "id": properties[self.feature_id],
+                }
+            )
+        return feature
+
+    def from_feature(self, feature):
+        properties = feature["properties"]
+        properties[self.opts.feature_geometry] = feature["geometry"]
+        return properties
+
+    @post_dump(pass_many=True)
+    def to_geojson(self, data, many, **kwargs):
+        if self.as_geojson:
+            if many:
+                return FeatureCollectionSchema().dump(
+                    {"features": [self.to_feature(props) for props in data]}
+                )
+            else:
+                return FeatureSchema().dump(self.to_feature(data))
+        else:
+            return data
+
+    @pre_load(pass_many=True)
+    def from_geojson(self, data, many, **kwargs):
+        if not self.as_geojson:
+            return data
+        if many:
+            collection = FeatureCollectionSchema(partial=False, unknown=RAISE).load(data)
+            return [self.from_feature(feature) for feature in collection["features"]]
+        else:
+            feature = FeatureSchema(partial=False, unknown=RAISE).load(data)
+            return self.from_feature(feature)
---
+++ src/utils_flask_sqla_geo/tests/test_schema.py
@@ -0,0 +1,433 @@
+import pytest
+
+import marshmallow as ma
+from marshmallow.exceptions import ValidationError
+import sqlalchemy as sa
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from geoalchemy2 import Geometry
+from geoalchemy2.shape import from_shape, to_shape
+from shapely.geometry import Point
+
+from utils_flask_sqla.schema import SmartRelationshipsMixin
+from utils_flask_sqla_geo.schema import GeoAlchemyAutoSchema
+
+
+# TODO:
+# - Test other geometries than Point & Polygon
+# - Test relationships
+# - Test geometry with third dimension
+
+
+Base = declarative_base()
+
+
+class Parent(Base):
+    __tablename__ = "parent"
+    pk = Column(Integer, primary_key=True)
+    name = Column(String)
+    geom = Column(Geometry("GEOMETRY", 4326))
+
+
+# class ParentSchema(SmartRelationshipsMixin, GeoAlchemyAutoSchema):
+class ParentSchema(GeoAlchemyAutoSchema):
+    class Meta:
+        model = Parent
+        include_fk = True
+        feature_id = "pk"
+
+
+@pytest.fixture
+def p1():
+    return Parent(pk=1, name="p1", geom=from_shape(Point(6, 10)))
+
+
+@pytest.fixture
+def p2():
+    return Parent(pk=2, name="p2", geom=from_shape(Point(10, 6)))
+
+
+class TestGeoSchema:
+    def test_to_json(self, p1):
+        expected = {
+            "pk": 1,
+            "name": "p1",
+            # geom field is automatically excluded
+        }
+        assert ParentSchema().dump(p1) == expected
+        assert ParentSchema(only=["geom"]).dump(p1) == {
+            "geom": {
+                "type": "Point",
+                "coordinates": (6.0, 10.0),
+            },
+        }
+        assert ParentSchema(exclude=["pk"]).dump(p1) == {"name": "p1"}
+        assert ParentSchema(only=["name", "geom"], exclude=["pk", "geom"]).dump(p1) == {
+            "name": "p1"
+        }
+
+    def test_to_json_many(self, p1, p2):
+        expected = [
+            {
+                "pk": 1,
+                "name": "p1",
+            },
+            {
+                "pk": 2,
+                "name": "p2",
+            },
+        ]
+        assert ParentSchema().dump([p1, p2], many=True) == expected
+        assert ParentSchema(only=["pk", "geom"], exclude=["geom"]).dump([p1], many=True) == [
+            {"pk": 1}
+        ]
+        assert ParentSchema(exclude=["pk"]).dump([p2], many=True) == [{"name": "p2"}]
+        assert ParentSchema(only=["pk", "geom"], exclude=["pk"]).dump([p1], many=True) == [
+            {"geom": {"type": "Point", "coordinates": (6, 10)}}
+        ]
+
+    def test_to_geojson_feature(self, p1):
+        expected = {
+            "id": 1,
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": (6.0, 10.0),
+            },
+            "properties": {
+                "pk": 1,
+                "name": "p1",
+            },
+        }
+        assert ParentSchema(as_geojson=True).dump(p1) == expected
+        expected = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": (6.0, 10.0),
+            },
+            "properties": {
+                "name": "p1",
+            },
+        }
+        assert ParentSchema(as_geojson=True, exclude=["pk", "geom"]).dump(p1) == expected
+        expected = {
+            "id": 1,
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": (6.0, 10.0),
+            },
+            "properties": {
+                "pk": 1,
+            },
+        }
+        assert ParentSchema(as_geojson=True, only=["pk", "geom"]).dump(p1) == expected
+        assert ParentSchema(as_geojson=True, only=["pk"], exclude=["geom"]).dump(p1) == expected
+
+    def test_to_geojson_feature_collection(self, p1, p2):
+        expected = {
+            "type": "FeatureCollection",
+            "features": [
+                {
+                    "id": 1,
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": (6.0, 10.0),
+                    },
+                    "properties": {
+                        "pk": 1,
+                        "name": "p1",
+                    },
+                },
+                {
+                    "id": 2,
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                        "coordinates": (10.0, 6.0),
+                    },
+                    "properties": {
+                        "pk": 2,
+                        "name": "p2",
+                    },
+                },
+            ],
+        }
+        assert ParentSchema(as_geojson=True).dump([p1, p2], many=True) == expected
+
+    def test_from_json(self):
+        p1 = ParentSchema().load(
+            {
+                "name": "p1",
+            }
+        )
+        p1 = ParentSchema().load(
+            {
+                "pk": 1,
+                "name": "p1",
+            }
+        )
+
+    def test_from_json_unexpected_geom(self):
+        with pytest.raises(ValidationError, match="'geom': \\['Unknown field.'\\]"):
+            ParentSchema().load(
+                {
+                    "pk": 1,
+                    "name": "p1",
+                    "geom": {
+                        "type": "Point",
+                        "coordinates": (6.0, 10.0),
+                    },
+                }
+            )
+
+    def test_from_geojson_feature(self):
+        p1 = ParentSchema(as_geojson=True).load(
+            {
+                "id": 1,
+                "type": "Feature",
+                "geometry": {
+                    "type": "Point",
+                    "coordinates": (6.0, 10.0),
+                },
+                "properties": {
+                    "pk": 1,
+                    "name": "p1",
+                },
+            }
+        )
+        assert p1["pk"] == 1
+        assert p1["name"] == "p1"
+        assert to_shape(p1["geom"]).equals(Point(6, 10))
+
+    def test_from_geojson_feature_collection(self):
+        p1, p2 = ParentSchema(as_geojson=True).load(
+            {
+                "type": "FeatureCollection",
+                "features": [
+                    {
+                        "id": 1,
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": (6.0, 10.0),
+                        },
+                        "properties": {
+                            "pk": 1,
+                            "name": "p1",
+                        },
+                    },
+                    {
+                        "id": 2,
+                        "type": "Feature",
+                        "geometry": {
+                            "type": "Point",
+                            "coordinates": (10.0, 6.0),
+                        },
+                        "properties": {
+                            "pk": 2,
+                            "name": "p2",
+                        },
+                    },
+                ],
+            },
+            many=True,
+        )
+        assert p1["pk"] == 1
+        assert p1["name"] == "p1"
+        assert to_shape(p1["geom"]).equals(Point(6, 10))
+        assert p2["pk"] == 2
+        assert p2["name"] == "p2"
+        assert to_shape(p2["geom"]).equals(Point(10, 6))
+
+    def test_to_geojson_null_geom(self):
+        expected = {
+            "id": 1,
+            "type": "Feature",
+            "geometry": None,
+            "properties": {
+                "pk": 1,
+                "name": "p",
+            },
+        }
+        p = Parent(pk=1, name="p")
+        assert ParentSchema(as_geojson=True).dump(p) == expected
+
+    def test_to_geojson_excluded_geom(self, p1):
+        expected = {
+            "id": 1,
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": (6.0, 10.0),
+            },
+            "properties": {
+                "pk": 1,
+                "name": "p1",
+            },
+        }
+        assert ParentSchema(as_geojson=True, exclude=["geom"]).dump(p1) == expected
+
+    def test_from_geojson_null_geom(self):
+        p = ParentSchema(as_geojson=True).load(
+            {
+                "id": 1,
+                "type": "Feature",
+                "geometry": None,
+                "properties": {
+                    "pk": 1,
+                    "name": "p",
+                },
+            }
+        )
+
+    def test_from_geojson_missing_geom(self):
+        with pytest.raises(
+            ValidationError, match="'geometry': \\['Missing data for required field.'\\]"
+        ):
+            ParentSchema(as_geojson=True).load(
+                {
+                    "id": 1,
+                    "type": "Feature",
+                    "properties": {
+                        "pk": 1,
+                        "name": "p",
+                    },
+                }
+            )
+
+    def test_from_geojson_bad_geom(self):
+        with pytest.raises(
+            ValidationError, match="'type': \\['Missing data for required field.'\\]"
+        ):
+            ParentSchema(as_geojson=True).load(
+                {
+                    "id": 1,
+                    "type": "Feature",
+                    "geometry": {
+                        "invalid": "invalid",
+                    },
+                    "properties": {
+                        "pk": 1,
+                        "name": "p1",
+                    },
+                }
+            )
+        with pytest.raises(
+            ValidationError, match="'coordinates': \\['Missing data for required field.'\\]"
+        ):
+            ParentSchema(as_geojson=True).load(
+                {
+                    "id": 1,
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Point",
+                    },
+                    "properties": {
+                        "pk": 1,
+                        "name": "p1",
+                    },
+                }
+            )
+
+    def test_from_geojson_invalid_geom(self):
+        with pytest.raises(ValidationError, match="'geom': \\['Invalid geometry.'\\]"):
+            p = ParentSchema(as_geojson=True).load(
+                {
+                    "type": "Feature",
+                    "geometry": {
+                        "type": "Polygon",
+                        "coordinates": [
+                            [
+                                (0, 0),
+                                (0, 3),
+                                (3, 3),
+                                (3, 0),
+                                (2, 0),
+                                (2, 2),
+                                (1, 2),
+                                (1, 1),
+                                (2, 1),
+                                (2, 0),
+                                (0, 0),
+                            ]
+                        ],
+                    },
+                    "properties": {"pk": 1},
+                }
+            )
+
+    def test_feature_id(self):
+        class ModelA(Base):
+            __tablename__ = "table_a"
+            pk = Column(Integer, primary_key=True)
+            name = Column(String)
+            geom = Column(Geometry("GEOMETRY", 4326))
+
+        class ModelASchema(GeoAlchemyAutoSchema):
+            class Meta:
+                model = ModelA
+                include_fk = True
+
+        a = ModelA(pk=1, name="a1")
+        assert ModelASchema(as_geojson=True, only=["pk"]).dump(a) == {
+            "type": "Feature",
+            "geometry": None,
+            "properties": {"pk": 1},
+        }
+        assert ModelASchema(as_geojson=True, only=["pk"], feature_id="name").dump(a) == {
+            "type": "Feature",
+            "geometry": None,
+            "properties": {"pk": 1},
+        }
+        assert ModelASchema(as_geojson=True, only=["name"], feature_id="name").dump(a) == {
+            "id": "a1",
+            "type": "Feature",
+            "geometry": None,
+            "properties": {"name": "a1"},
+        }
+
+    def test_many_geometries(self):
+        class ModelB(Base):
+            __tablename__ = "table_b"
+            pk = Column(Integer, primary_key=True)
+            name = Column(String)
+            geom1 = Column(Geometry("GEOMETRY", 4326))
+            geom2 = Column(Geometry("GEOMETRY", 4326))
+
+        class ModelBSchema1(GeoAlchemyAutoSchema):
+            class Meta:
+                model = ModelB
+                include_fk = True
+
+        class ModelBSchema2(GeoAlchemyAutoSchema):
+            class Meta:
+                model = ModelB
+                include_fk = True
+                feature_geometry = "geom2"
+
+        ModelBSchema1(as_geojson=False)
+        with pytest.raises(TypeError, match="Missing 'feature_geometry'"):
+            ModelBSchema1(as_geojson=True)
+        schema1 = ModelBSchema1(as_geojson=True, feature_geometry="geom2")
+        ModelBSchema2(as_geojson=False)
+        schema2 = ModelBSchema2(as_geojson=True)
+
+        b = ModelB(
+            pk=1, name="b1", geom1=from_shape(Point(1.2, 2.5)), geom2=from_shape(Point(3.5, 4.8))
+        )
+        expected = {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": (3.5, 4.8),
+            },
+            "properties": {
+                "pk": 1,
+                "name": "b1",
+            },
+        }
+        assert schema1.dump(b) == expected
+        assert schema2.dump(b) == expected
